fc8fa72f83064efad41998d3bd8c03a06e6da802
Mikhail Nikolaev <nmichail154@mail.ru>
2024-07-06 18:51:22 +0700

    Complete first screen

--- app/src/main/java/com/example/spotify/presentation/firstScreen.kt
+++ app/src/main/java/com/example/spotify/presentation/firstScreen.kt
@@ -1,19 +1,94 @@
 package com.example.spotify.presentation
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.android.volley.toolbox.ImageLoader
+import com.example.spotify.R
+import com.example.spotify.ui.theme.SpotiGreen
 import com.example.spotify.ui.theme.fontFamily
 
 @Composable
 fun FirstScreen() {
-    Text(
-        text = "Hello",
-        fontFamily = fontFamily,
-        fontSize = MaterialTheme.typography.headlineLarge.fontSize,
-        modifier = Modifier.padding(100.dp)
-    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.firstscreen1),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Image(
+            painter = painterResource(id = R.drawable.spot_big),
+            contentDescription = "Spotify Logo",
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .size(180.dp)
+                .padding(start = 20.dp, bottom = 20.dp)
+        )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 340.dp, start = 16.dp)
+        ) {
+
+            Text(
+                text = "Enjoy Listening To Music",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            Text(
+                text = "Spotify is a music streaming service that offers users access to a vast library of millions of songs, albums, and playlists from artists all around the world.",
+                fontSize = 14.sp,
+                color = Color.White.copy(alpha = 0.7f),
+                modifier = Modifier.padding(bottom = 32.dp)
+            )
+
+            Button(
+                onClick = { },
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = SpotiGreen
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+            ) {
+                Text(text = "Get started", color = Color.White, fontSize = 16.sp)
+            }
+        }
+    }
 }
